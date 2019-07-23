32c891fb0a21c185c4305041c86da1c962ce3935
taylor-meyer <taylor.ross.meyer@gmail.com>
2019-07-23 03:00:42 -0700

    Add and print intro

--- driver.cpp
+++ driver.cpp
@@ -15,14 +15,17 @@ as a way to learn unity and get some 2D graphics going!
 
 #include <iostream>
 #include <string>
+#include <vector> // might need
 
 
 int main()
 {
 
-	std::string hello_world = "Hello, world!\n";
+	std::string intro = "Welcome to my recreation of 21 from the "
+		"Resident Evil 7 DLC.\nEverything is bare bones for now, "
+		"but I will do my best to implement the rest of the features!\n";
 
-	std::cout << hello_world;
+	std::cout << intro << std::endl;
 
 
 	// sys(pause) is bad practice but we are just playing for now.
