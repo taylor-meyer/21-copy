fc9c1fa4110fe51b0178d8bce4d304cb058a7470
taylor-meyer <taylor.ross.meyer@gmail.com>
2019-07-23 02:56:02 -0700

    Initial commit

---
+++ driver.cpp
@@ -0,0 +1,32 @@
+
+/*
+
+07/23/2019
+
+I was playing the 21 dlc for Resident Evil 7
+and thought, "I can probably re-create this!"
+
+I'll start with text base, and then I can use this
+as a way to learn unity and get some 2D graphics going!
+
+:)
+
+*/
+
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+
+	std::string hello_world = "Hello, world!\n";
+
+	std::cout << hello_world;
+
+
+	// sys(pause) is bad practice but we are just playing for now.
+	system("PAUSE");
+
+
+}
