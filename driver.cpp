ff8f71b390f91824d660d97343a7a6042f3e1217
taylor-meyer <taylor.ross.meyer@gmail.com>
2019-07-31 04:51:08 -0700

    Read and write win/loss stats to file

--- driver.cpp
+++ driver.cpp
@@ -4,53 +4,70 @@ Code started:
 07/23/2019
 
 Last edit:
-07/30/2019
+07/31/2019
 
 I was playing the 21 dlc for Resident Evil 7
 and thought, "I can probably re-create this!"
 
 I'll start with text base, and then I can use this
-as a way to learn unity and get some 2D graphics going!
+as a way to learn Unity and get some 2D graphics going!
 
 :)
 
 */
 
 #include "ClearScreen.h" // ClearScreen()
-#include "Hand.h"
-#include <iostream>
-#include <time.h>
-#include <thread> //std::this_thread::sleep_for
+#include "Hand.h" // Opponent/Player/Shoe
 #include <chrono> //std::chrono::seconds
+#include <fstream> // Read/write stats
+#include <iostream> // std::cout,std::cin
+#include <time.h> // Seed rand
+#include <thread> //std::this_thread::sleep_for
 
 // Function prototypes
 int menu();
 
-
+// Main
 int main()
 {
 	// Seed random
-	srand(time(NULL));
+	srand((unsigned int)time(NULL));
+
+	// Stats
+	int wins = 0;
+	int loss = 0;
+
+	// File for stats
+	std::string line;
+	std::ifstream infile;
+	infile.open("stats.dat", std::ios::in);
+	infile >> wins >> loss; // Load stats
+	infile.close();
 
 	// Introduction
 	std::string intro = "Welcome to my recreation of 21 from the "
 		"Resident Evil 7 DLC.\nEverything is bare bones for now, "
-		"but I will do my best to implement the rest of the features!\n";
+		"but I will do my best to implement the rest of the features!";
 	// Rules
 	std::string rules = "Computer opponent will stay on 17 and will not reveal their hand until you stay or bust.\n"
-		"If both you and opponent bust, smaller count wins.\n";
+		"If both you and opponent bust, smaller count wins.";
 	// Printout
-	std::cout << intro << rules << std::endl;
+	std::cout << intro << std::endl << rules << std::endl << std::endl;
+
+	// Print stats
+	std::cout << "Wins: " << wins << "\nLosses: " << loss << std::endl << std::endl;
 
 	// Declare shoe and players
 	Hand shoe; shoe.fillShoe(); // Shoe with all cards loaded
 	Hand p1; // Player
 	Hand opp; // Opponent
+	
 
 	// Menu
 	switch (menu())
 	{
 	case 1:
+	{
 		while (true)
 		{
 			ClearScreen();
@@ -113,7 +130,10 @@ int main()
 				||
 				(p1.sum() > 21 && opp.sum() > 21 && p1.sum() < opp.sum())
 				)
+			{
 				std::cout << "You win!\n\n";
+				wins++;
+			}
 			// Opponent wins
 			else if
 				(
@@ -121,7 +141,10 @@ int main()
 					||
 					(opp.sum() > 21 && p1.sum() > 21 && opp.sum() < p1.sum())
 					)
+			{
 				std::cout << "Opponent wins!\n\n";
+				loss++;
+			}
 			else
 				std::cout << "ERROR, ELSE-IF WIN CONDITIONS!\n\n";
 
@@ -139,11 +162,13 @@ int main()
 			}
 		}
 
-		//std::cout << "Shoe:\n" << shoe.toString() << "\n   Sum: " << shoe.sum() << std::endl << std::endl;
+		// Write new stats
+		std::ofstream outfile;
+		outfile.open("stats.dat", std::ofstream::out | std::ofstream::trunc);
+		outfile << wins << std::endl << loss;
+		outfile.close();
+	}
 
-		// sys(pause) is bad practice but we are just playing for now
-		//system("PAUSE");
-		//exit(0);
 
 	case 0:
 		std::cout << "Exiting...\n";
@@ -157,17 +182,9 @@ int main()
 
 
 
-	std::cout << "Shoe:\n" << shoe.printReveal() << "\n   Sum: " << shoe.sum() << std::endl << std::endl;
-
-	std::cout << "P1:\n" << p1.printReveal() << "\n   Sum: " << p1.sum() << std::endl << std::endl;
-
-	//std::cout << "P2:\n" << p2.toString() << "\n   Sum: " << p2.sum() << std::endl << std::endl;
-
 
 	// sys(pause) is bad practice but we are just playing for now
 	system("PAUSE");
-
-
 }
 
 // Prints the menu and returns what user wants to do in form of an integer
